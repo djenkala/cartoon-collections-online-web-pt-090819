858c34b8b73d3f451912db8b19017634e3546c04
Daniel Jenkala <djenkala@yahoo.com>
2019-09-13 15:14:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planateer_calls)
-  planateer_calls.map { |calls| calls. }
+  planateer_calls.map { |calls| calls.capatilize + '!' }
   
 end
 
