330e353e1004ea9d72698429300d45ea5592cf13
Daniel Jenkala <djenkala@yahoo.com>
2019-09-13 15:40:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,10 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
-    puts "#{index + 1}. #{dwarf}!"
+    puts "#{index + 1}. #{dwarf}"
   end
 end
+
+roll_call_dwarves(dwarves)
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
